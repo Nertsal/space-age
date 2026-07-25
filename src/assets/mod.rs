43fee91451bb4c1f97d1e48380896ee8ebab6717
nertsal <nestralnertsal@gmail.com>
2026-07-25 20:53:14 +0300

    load new sprites

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -90,4 +90,42 @@ pub struct SpritesUi {
     pub border: Rc<ugli::Texture>,
     #[load(options(filter = "ugli::Filter::Nearest"))]
     pub fill: Rc<ugli::Texture>,
+
+    #[load(options(filter = "ugli::Filter::Nearest"))]
+    pub scientific_research: Rc<ugli::Texture>,
+    #[load(options(filter = "ugli::Filter::Nearest"))]
+    pub theoretic_research: Rc<ugli::Texture>,
+
+    pub research: SpritesResearch,
+}
+
+#[derive(geng::asset::Load)]
+pub struct SpritesResearch {
+    #[load(options(filter = "ugli::Filter::Nearest"))]
+    pub orbit_program: Rc<ugli::Texture>,
+}
+
+impl SpritesResearch {
+    pub fn get_icon(&self, name: &str) -> &Rc<ugli::Texture> {
+        match name {
+            "Orbit Program" => &self.orbit_program,
+            "Crashlanding" => &self.orbit_program,
+            "Theorycrafting I" => &self.orbit_program,
+            "Theorycrafting II" => &self.orbit_program,
+            "Engineering I" => &self.orbit_program,
+            "Engineering II" => &self.orbit_program,
+            "Structural Integrity I" => &self.orbit_program,
+            "Structural Integrity II" => &self.orbit_program,
+            "Data Compression I" => &self.orbit_program,
+            "Data Compression II" => &self.orbit_program,
+            "Communications" => &self.orbit_program,
+            "Orbit Observation" => &self.orbit_program,
+            "Collision Risk Analysis" => &self.orbit_program,
+            "Debris Cleaning" => &self.orbit_program,
+            "Collision Avoidance" => &self.orbit_program,
+            "Automation I" => &self.orbit_program,
+            "Automation II" => &self.orbit_program,
+            _ => &self.orbit_program,
+        }
+    }
 }
